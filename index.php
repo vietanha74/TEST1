5018221048a674449cf33f5c45a78d5591a155f4
Vietanh <vietanha74@gmail.com>
2018-01-02 15:04:20 +0700

    Change file

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<h1>123<h1>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+    background-color: lightblue;
+}
+
+h1 {
+    color: white;
+    text-align: center;
+}
+
+p {
+    font-family: verdana;
+    font-size: 20px;
+}
+</style>
+</head>
+<body>
+
+<h1>Viet Anh Site</h1>
+<p>NOTHING</p>
+
+</body>
+</html>
